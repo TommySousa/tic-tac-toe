509f5509725e0b41dfa002df4b690511ee614f6f
Tomás Sousa <118855300+TommySousa@users.noreply.github>
2023-01-12 12:13:55 +0000

    Redoing most of the JS

--- index.js
+++ index.js
@@ -2,35 +2,73 @@
 
 const GameBoard = (() => {
     let gameBoard =  ['', '', '' ,'', '', '', '', '', ''];
+    let tiles = document.querySelectorAll('.tile');
+    let index =-1;
 
+   
     const renderPlay = () => {
-        let tiles = document.querySelectorAll('.tile');
         for(let i = 0; i<tiles.length; i++){
-            tiles[i].innerHTML = gameBoard[i]
+            tiles[i].innerHTML = gameBoard[i];
         };     
     };
-    return {gameBoard,renderPlay};
+
+    const clickedSquare = () => {
+        for (let i = 0; i<tiles.length; i++){
+            tiles[i].addEventListener('click', () => {
+                console.log(`You clicked tile number ${i}`)
+                return i;
+            })
+        }
+    };
+
+    //  const findClicked = (e) => {
+    //      let target = e.target;
+    //      index = e.target.id;
+    //      console.log(target);
+    //      console.log(index)
+    //      e.target.removeAttribute("onclick");
+    //  };
+    
+    return {gameBoard,renderPlay, clickedSquare};
 })();
 
-//win logic check if array spaces are 3 in a row, but only from round 5 
 
 
-const Player = (symbol) => {
+
+
+const Player = (symbol, name) => {
     const getSymbol = () => symbol;
-     
+    const getName = () => name;
+
     const playSquare = () => {
-        let tiles = document.querySelectorAll('.tile')
-        tiles.forEach(tile => {
-            tile.addEventListener('click', (e) => {
-                console.log(e.target.id)
-                GameBoard.gameBoard.splice(e.target.id, 1, getSymbol());
-                console.log(GameBoard.gameBoard);
-                GameBoard.renderPlay();  
-            });
-        });
-     };
+        GameBoard.gameBoard.splice(GameBoard.index, 1, getSymbol())
+        GameBoard.renderPlay();
+        console.log(`${getName()} played `)
+    }
+
+    // const playSquare = () => {
+    //     let tiles = document.querySelectorAll('.tile')
+    //      tiles.forEach(tile => {
+    //          tile.addEventListener('click', (e) => {
+    //                  console.log(e.target.id)
+    //                  GameBoard.gameBoard.splice(e.target.id, 1, getSymbol());
+    //                  console.log(GameBoard.gameBoard)
+    //                  GameBoard.renderPlay();
+    //          }, {once:true});
+    //  });
+    // }; 
+
+
+     
+    // const playSquare = (index) => {
+    //     if (index >=0){
+    //         GameBoard.gameBoard.splice(index, 1, getSymbol());
+    //         GameBoard.renderPlay();
+    //     }
+    //   };
 
     return {playSquare};
+    
 };
 
 const Game = ((player1, player2) => {
@@ -40,18 +78,33 @@ const Game = ((player1, player2) => {
 
     const playRound = () => {
         if(player === 1){
-            round = round+1;
             player1.playSquare();
             player = 2;
+            round = round+1;
 
         } else if (player === 2){
-            round = round+1;
             player2.playSquare();
             player = 1;
+            round = round+1;
         }
     };
-
-
-return {playRound};
     
-})(Player('X'), Player('0'));
+    const checkWinner = () =>{
+        let gameBoard = JSON.stringify(GameBoard.gameBoard);
+        if(gameBoard === (JSON.stringify([ "&#10005", "&#10005", "&#10005", "", "", "", "", "", ""]) || JSON.stringify([ "", "", "", "&#10005", "&#10005", "&#10005", "", "", ""]) || JSON.stringify([ "", "", "", "", "", "", "&#10005", "&#10005", "&#10005"]) || JSON.stringify([ "&#10005", "", "", "&#10005", "", "", "&#10005", "", ""]) || JSON.stringify([ "", "&#10005", "", "", "&#10005", "", "", "&#10005", ""]) || JSON.stringify([ "", "", "&#10005", "", "", "&#10005", "", "", "&#10005"]) || JSON.stringify([ "&#10005", "", "", "", "&#10005", "", "", "", "&#10005"]) || JSON.stringify([ "", "", "&#10005", "", "&#10005", "", "&#10005", "", ""]))){
+            console.log('You win!')
+            return true
+        }
+        else if (gameBoard === (JSON.stringify([ "&#9675", "&#9675", "&#9675", "", "", "", "", "", ""]) || JSON.stringify([ "", "", "", "&#9675", "&#9675", "&#9675", "", "", ""]) || JSON.stringify([ "", "", "", "", "", "", "&#9675", "&#9675", "&#9675"]) || JSON.stringify([ "&#9675", "", "", "&#9675", "", "", "&#9675", "", ""]) || JSON.stringify([ "", "&#9675", "", "", "&#9675", "", "", "&#9675", ""]) || JSON.stringify([ "", "", "&#9675", "", "", "&#9675", "", "", "&#9675"]) || JSON.stringify([ "&#9675", "", "", "", "&#9675", "", "", "", "&#9675"]) || JSON.stringify([ "", "", "&#9675", "", "&#9675", "", "&#9675", "", ""]))){
+            console.log('You loose')
+            return true
+        } else{
+            console.log('False')
+        }
+
+    }
+
+return {round, playRound, checkWinner};
+    
+})(Player('&#10005', 'You'), Player('&#9675', 'Your Friend'));
+
